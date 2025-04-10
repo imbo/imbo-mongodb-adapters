67284f3175175d6085b4182d47cb48f780d3b50a
Christer Edvartsen <cogo@starzinger.net>
2025-04-10 20:18:44 +0200

    chore: Update config

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,8 +1,9 @@
 <?php declare(strict_types=1);
 require 'vendor/autoload.php';
 
-$finder = (new \PhpCsFixer\Finder())
-    ->in(__DIR__);
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->append([__FILE__]);
 
-return (new \Imbo\CodingStandard\Config())
+return (new Imbo\CodingStandard\Config())
     ->setFinder($finder);
